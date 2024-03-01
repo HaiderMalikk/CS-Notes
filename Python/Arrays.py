e5c9cc313e10a314a66b7be9c38c0dfaaeaea3c2
HaiderMalikk <haidermalik662@gmail.com>
2024-02-29 22:35:39 -0500

    OLD REPO CLOSED

--- Python/Arrays.py
+++ Python/Arrays.py
@@ -459,4 +459,68 @@ else:
 
 
 
+# radome guess
+import random as rd
 
+# def game():
+#     print(f"helo welcom to gess game pick num 1-10 hehe")
+#     num = (input("pick a number from 1-10: ")) # can do num(input)
+#     num = int(num)
+#     if num >= 1 and num <= 10:
+#         rdnum = rd.randint(1, 10)
+#         if rdnum == num:
+#             print("you win you are the best player everrrr!!!!!")
+        
+#         else: print(f"you lose the num was: {rdnum}")
+#     else:
+#         print('please enter a valid number')
+#         game()
+
+# game()
+
+# class Student:
+#     def __init__(self, name, major, student_num):
+#         self.name = name
+#         self.major = major
+#         self.student_num = student_num
+#     def greet(self):
+#         return f"hello my name is {self.name} my major is {self.major} my student id is {self.student_num}"
+    
+# stu1 = Student("john", "english", "23456789")
+
+
+# list = [stu1.name, stu1.major, stu1.student_num]
+
+# for i in list:
+#     print(i)
+
+# greeting = stu1.greet()
+# print(greeting)
+
+# class
+# class arrays:
+#     def array():
+#         arr = []
+#         n = int(input("how many numbers u want to add? :"))
+#         for i in range(n): # for loop runs until n integer starting from 0 if n=2 the for loop runs 2 times
+#             # i+1 in {} displayes the number of the number being added so on the second number it will say enter number 2 
+#             # but why i+1? becuase i starts at zero like mentioned before there for on the first run the number should be num1 not 0
+#             x = input(f"enter a number{i+1}: ")
+#             arr.append(int(x))
+#         return arr
+
+# # list comprihention
+# arr = [[1, 2, 3], [1, 3, 2], [3, 2, 1]]
+
+# # Create a new list to store the modified sublists
+# new_arr = []
+# count = 0
+# # the for value in sublist is the second loop 
+# # if value != 3 is our condition 
+# # value is just the variable like J
+# # value is just a variable name that represents each element in the sublist 
+# for sublist in arr:
+#     new_sublist = [value for value in sublist if value != 3]
+#     new_arr.append(new_sublist)
+
+# print(new_arr)
